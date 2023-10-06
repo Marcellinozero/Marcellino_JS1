5d19bad64383c31c24c273a7dbc151488b0d2fa7
Marcellino <macellinow@gmail.com>
2023-10-07 01:03:12 +0700

    Adding New JS Project Folder

---
+++ Marcellino_JS1/json.js
@@ -0,0 +1,68 @@
+// // If
+// let a = 5
+// if(a <= 10){
+//     a++;
+//     console.log(a);
+// }
+
+// // If, Else
+// let b = 5
+// if(b === 5){
+//     console.log("Ini angka 5")
+// }else {
+//     console.log("Ini bukan angka 5")
+// }
+
+// // nested if
+// let x = 3
+// let y = 3
+// if(x < 5){
+//     console.log("Nomor X anda lebih kecil dari 5");
+//         if(y < 5){
+//             console.log("Nomor Y anda lebih kecil dari 5");
+//         }else{
+//             console.log("Nomor Y anda lebih besar dari 5")
+//         }
+// }else{
+//     console.log("Nomor x anda lebih besar dari 5")
+// }
+
+
+
+// // Switch
+// let game = "valorant"
+// switch(game){
+//     case "valorant":
+//         console.log("Ini merupakan game fps");
+//         break;
+//     case "phasmaphobia":
+//         console.log("Ini Game Hantu");
+//         break;
+//     default:
+//         console.log("Gtw game apa ini");
+// } 
+
+// // For
+// for (let a = 0; a < 3; a++){
+//     console.log(a);
+// }
+
+// // while
+// let c = 0
+// while (c <= 5){
+//     console.log(c)
+//     c++;
+// }
+
+// // while do
+// let c = 0
+// do{
+//     console.log(c);
+//     c++;
+// }while(c <= 5);
+
+// // function
+// let fun = test(5,10)
+// function test(a,b){
+//     console.log(b/a);
+// }
